9a24390187a679af516e3a5edef6400adc97eb7c
wayson <wayson317@msn.com>
2016-11-13 17:07:21 +1100

    Category CRUD

---
+++ src/AppBundle/Controller/CategoryController.php
@@ -0,0 +1,99 @@
+<?php
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * Class CategoryController
+ * @package AppBundle\Controller
+ *
+ * @Route("/admin/category")
+ */
+class CategoryController extends Controller
+{
+    /**
+     * @Route("/", name="category_list")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $categories = $this->getDoctrine()->getRepository(Category::class)->getUserCategories($this->getUser()->getId());
+
+        return $this->json($categories);
+    }
+
+    /**
+     * @Route("/create", name="category_create")
+     * @Method("POST")
+     */
+    public function createAction(Request $request)
+    {
+        $content = json_decode($request->getContent(), true);
+
+        $category = new Category();
+        $category->setName($content['name']);
+        $category->setUserId($this->getUser()->getId());
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($category);
+        $em->flush();
+
+        return $this->json(['result' => true]);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @Route("/edit/{categoryId}", name="category_edit")
+     * @Method("PUT")
+     */
+    public function editAction(Request $request)
+    {
+        $content = json_decode($request->getContent(), true);
+
+        $categoryId = $request->get('categoryId');
+        
+        $category = $this->getDoctrine()->getRepository(Category::class)->getCategoryByIdAndUser(
+            $categoryId,
+            $this->getUser()->getId()
+        );
+
+        $category->setName($content['name']);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($category);
+        $em->flush();
+
+        return $this->json(['result' => true]);
+
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @Route("/delete/{categoryId}", name="category_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request)
+    {
+        $categoryId = $request->get('categoryId');
+
+        $category = $this->getDoctrine()->getRepository(Category::class)->getCategoryByIdAndUser(
+            $categoryId,
+            $this->getUser()->getId()
+        );
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($category);
+        $em->flush();
+
+        return $this->json(['result' => true]);
+    }
+
+}
---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="category")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return Category
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
